e9084a9775596896d390b8c82c2cd7bc6b2e1b74
Adam <sykesa5@mymacewan.ca>
2015-01-30 13:59:34 -0500

    Changed the about page to include rules for the boards and the corresponding style sheet

--- Lab1/app/views/about.blade.php
+++ Lab1/app/views/about.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts/master')
 @section('content')
-	<p> Copyright 2015 by Adam Sykes and Chris Dubeau. All rights reserved.</p>
+{{HTML::style(asset('css/aboutcss.css'))}}
+	<div id='writing'>
+	    <p> This website is a community forum for discussions on the current generation of video game consoles.
+	    Users can post about anything, so long as it is related to the current board.</p> <p>Guests will not be able to post
+	    on the boards but will be able to see the discussions being held. This website is meant to be non-serious 
+	    and relaxing, so we've come up with a few rules to ensure this.</p>
+	</div>
+	<h4>Rules</h4>
+	<ul>
+	    <li>Keep posts on topic.</li>
+	    <li>No Profanity</li>
+	    <li>Don't argue for the sake of it. We want constructive discussion</li>
+	    <li>No flame wars between consoles. We have multiple boards for a reason</li>
+	    <li>All tributes to the gods must be done outside, and with a proper sacrifice.</li>
+	    <li>Turn your blinker off if you aren't turning. It just confuses other drivers</li>
+	    <li>No horseplay. I don't wanna have to call an ambulance.</li>
+	    <li>No running in the pool area.</li>
+	    <li>All flashbacks must be accompanied by soft music and sepia tone. I will get on your case about this.</li>
+	    <li>Spammers will get the axe. Not banned, I will literally throw an axe through the screen at you.</li>
+	    <li>If this is your first night at fight club, you must fight.</li>
+	</ul>
+	<h4>If you are caught breaking these rules, we'll delete your account. Just don't do it.</h4></p>
+	
+	<p>Copyright 2015 by Adam Sykes and Chris Dubeau. All rights reserved.</p>
 @stop
